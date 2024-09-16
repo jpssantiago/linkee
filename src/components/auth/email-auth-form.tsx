e64434cd9813a95ef2a32e3f4aeefada80be458b
João Pedro Santiago <joao@joaosantiago.com.br>
2024-09-15 22:50:18 -0300

    feat: error handling when the verification email could not be sent

--- src/components/auth/email-auth-form.tsx
+++ src/components/auth/email-auth-form.tsx
@@ -4,10 +4,10 @@ import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { signIn } from "next-auth/react"
+import { toast } from "sonner"
 
 import { Input } from "@/components/ui/input"
 import { LoadingButton } from "@/components/ui/loading-button"
-import { toast } from "sonner"
 
 const emailAuthSchema = z.object({
     email: z.string().email({ message: "This email doesn't look valid" })
@@ -21,17 +21,21 @@ export function EmailAuthForm() {
     })
 
     async function onSubmit({ email }: EmailAuthSchemaType) {
-        const response = await signIn("email", { email, redirect: false })
-
-        if (!response) {
+        try {
+            const response = await signIn("email", { email, redirect: false })
+    
+            if (!response) {
+                return toast.error("Unknown error. Please try again later.")
+            }
+            
+            if (response.error) {
+                return toast.error(response.error)
+            }
+            
+            toast.success("Email sent - Check your inbox (or spam).")
+        } catch {
             return toast.error("Unknown error. Please try again later.")
         }
-
-        if (response.error) {
-            return toast.error(response.error)
-        }
-
-        toast.success("Email sent - Check your inbox (or spam).")
     }
 
     return (
